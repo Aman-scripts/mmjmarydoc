cff8829a4b3607a6c499d468b0c6a2a98b423491
Amandeep <amandeep.nugen@gmail.com>
2026-07-15 17:04:49 +0530

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <ProcessSection />
       <FeaturesSection />
       <ValuesSection />
-      <JudgmentSection />
+      {/* <JudgmentSection /> */}
     </div>
   );
 }
