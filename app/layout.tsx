6be55a33e12f0f4f417816ef3b380c783fe7acd1
Amandeep <amandeep.nugen@gmail.com>
2026-08-24 16:34:52 +0530

    feat: add Google Search Console verification meta tag and GTM tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ScrollToTopOnLoad } from "@/components/scroll-to-top-on-load";
@@ -24,6 +25,9 @@ export const metadata: Metadata = {
   title: "Marydoc",
   description:
     "MaryDoc connects you with licensed physicians for secure online medical marijuana evaluations.",
+  verification: {
+    google: "deGWDcGfkhgxV6bmW670CPyLyA-Wiqj9u-5UzS5ZsXg",
+  },
 };
 
 export default function RootLayout({
@@ -36,7 +40,30 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", plusJakartaSans.variable, spaceGrotesk.variable, "font-sans")}
     >
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KL827468');`,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KL827468"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <script
           dangerouslySetInnerHTML={{
             __html: `if("scrollRestoration"in history)history.scrollRestoration="manual";window.scrollTo(0,0);`,
